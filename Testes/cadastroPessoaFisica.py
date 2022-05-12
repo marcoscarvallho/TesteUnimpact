3af48d7ecf55568ffb74bd41c67734d25edf561d
Julia Amato <juliaamato10@gmail.com>
2022-05-11 23:11:45 -0300

    juntando

---
+++ Testes/cadastroPessoaFisica.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+
+class CadastroPessoaFisica():
+    
+    def __init__(self, driver):
+        self.driver = driver
+        self.inicioCadastro()
+
+    def inicioCadastro (self):
+        self.driver.find_element_by_xpath('//div[contains(text(), "Pessoa Física")]').click()
+        self.inserirInputs()
+        self.inserirTelefone()
+
+    def inserirInputs(self):
+        campoLogin = self.driver.find_element_by_id('login')
+        campoLogin.clear()
+        campoLogin.send_keys('teste')
+        campoSenha = self.driver.find_element_by_id('password')
+        campoSenha.clear()
+        campoSenha.send_keys('teste2')
+        campoEmail = self.driver.find_element_by_id('email')
+        campoEmail.clear()
+        campoEmail.send_keys('Email')
+        campoNome = self.driver.find_element_by_id('nome')
+        campoNome.clear()
+        campoNome.send_keys('Testinho bonitinho')
+        campoCpf = self.driver.find_element_by_id('cpf')
+        campoCpf.clear()
+        campoCpf.send_keys('12783092417')
+    
+    def inserirTelefone(self):
+        campoTelefone = self.driver.find_element_by_id('telefone')
+        campoTelefone.clear()
+        campoTelefone.send_keys('+5581998329317')
+
+    
+
+
+
+
+
--- Testes/main.py
+++ Testes/main.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 import time
 
+from cadastroPessoaFisica import CadastroPessoaFisica
 
 driver = webdriver.Chrome()
 driver.get('https://unimpact2.herokuapp.com/lista-demandas')
 time.sleep(5)
 driver.find_element_by_xpath('//span[contains(text(), "Cadastro")]').click()
+
+CadastroPessoaFisica(driver)
