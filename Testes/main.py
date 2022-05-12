89da4075145d9cfd03b771f7f081de940d7c983b
Marcos Carvalho <marcosarcarvallho@gmail.com>
2022-05-11 23:14:10 -0300

    cadastrofinal

--- Testes/main.py
+++ Testes/main.py
@@ -1,14 +1,12 @@
 from selenium import webdriver
 import time
 import sys
+from cadastroPessoaJuridica import CadastroPessoaJuridica
 
-driver = webdriver.Chrome()
-driver.get('https://unimpact2.herokuapp.com/lista-demandas')
-time.sleep(1)
-driver.find_element_by_xpath('//span[contains(text(), "Cadastro")]').click()
-print("vai tentar agora")
-time.sleep(1)
-driver.find_element_by_xpath('//div[contains(text(), "Pessoa Jurídica")]').click()
-time.sleep(2)
-s = driver.find_elements_by_xpath("//input[@type='file']")
-s[0].send_keys("C:\\Users\\moaci\\OneDrive\\Área de Trabalho\\tcc marcos\\TesteUnimpact\\Testes\\imgtest.jpg")
+class main():
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(options=options)
+    driver.get('https://unimpact2.herokuapp.com/lista-demandas')
+    time.sleep(1)
+    CadastroPessoaJuridica(driver)
