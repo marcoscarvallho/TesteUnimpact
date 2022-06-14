79aa65b9123b8892772b16168ca4b6b5633fae8a
Marcos Carvalho <marcosarcarvallho@gmail.com>
2022-06-14 01:42:41 -0300

    ApagarDemanda

--- Testes/apagarDemanda.py
+++ Testes/apagarDemanda.py
@@ -10,8 +10,9 @@ class ApagarDemanda():
     
     def apagar(self, demanda):
         AcompanharDemandas(self.driver, demanda)
-        time.sleep(1)
-        botaoApagar = Esperas.porXpath(self, '//span[contains(text(), "Apagar")]')
+        time.sleep(3)
+        botaoApagar = Esperas.porXpath(self, '//span[contains(text(), "")]')
+        assert botaoApagar == None, 'Falha ao deletar a demanda'
         botaoApagar.click()
         botaoConfirmar = Esperas.porXpath(self, '//span[contains(text(), "Confirmar")]')
         botaoConfirmar.click()
